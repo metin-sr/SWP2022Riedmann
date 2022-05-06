7218ed62d499492965553d338e038b45f8403d19
metin-sr <kesermetin9@gmail.com>
2022-05-06 11:43:08 +0200

    STarted Cäsar

---
+++ src/at/sabo/projects/Caesar.java
@@ -0,0 +1,20 @@
+package at.sabo.projects;
+
+import java.util.Scanner;
+
+public class Caesar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Was wollen Sie verschlüsseln?");
+        String locked = scanner.nextLine();
+        System.out.println("Um wie viele stellen soll verschoben werden? (max 3)");
+
+        int shift = scanner.nextInt();
+
+
+        if (shift > 4 ) {
+            char []
+        }
+
+    }
+}
